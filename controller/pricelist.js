81ffe2feb1264af2b498ccee6bdc4dbbec8d893a
leoguty91 <leoguty91@gmail.com>
2014-02-18 13:44:05 -0300

    Se creo el factory 'priceFactory' y se lo agrego al pricelis.js

--- controller/pricelist.js
+++ controller/pricelist.js
@@ -2,29 +2,10 @@
  * Created by Diego Reyes on 1/29/14.
  */
 function pricelistCtrl($scope, priceFactory){
-	//'use strict';
+	'use strict';
 
 	priceFactory.getPrice(function (data) {
 		$scope.pricelist = data;
 	});
-
-	/*priceFactory.getPrice().success(function (data){
-		console.log("Se cargo la lista PriceList");
-		$scope.pricelist = data
-	}).error(function(){
-			console.log("Error al cargar la lista PriceList")
-		})*/
-
-	/*var inserturl = serverUrl + '/agp/pricelist';
-	$http({
-		method: 'GET',
-		url: inserturl,
-		cache: $templateCache
-	}).success(function(data) {
-			console.log("success");
-			$scope.pricelist = data;
-		}).error(function(response) {
-			console.log("error");
-		});*/
 
 }
