a03177ab8aee174d528b0c222b652d5fd2d4fca8
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2014-02-21 10:24:14 -0300

    Corrección en el objeto que se envía a cargar desde el match y validación de los códigos que se insertan.

--- controller/invoices.modal.js
+++ controller/invoices.modal.js
@@ -1,8 +1,12 @@
 /**
  * Created by gutierrez-g on 20/02/14.
  */
-function invoicesModalCtrl($scope, $modalInstance, data, priceFactory){
+function invoicesModalCtrl($scope, $modalInstance, data){
 	'use strict';
+	$scope.factura = data.factura;
 
+	$scope.close = function(){
+		$modalInstance.dismiss('canceled');
+	}; // end cancel
 
 }
