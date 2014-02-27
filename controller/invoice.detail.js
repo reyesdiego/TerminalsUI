cefecced7070a9909655692743130b318768f257
Artiom <artiom.kolesnikov@gmail.com>
2014-02-27 12:31:03 -0300

    Separo el controlador del detalle de las facturas del main.

---
+++ controller/invoice.detail.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Artiom on 27/02/14.
+ */
+
+function invoiceDetailCtrl ($stateParams, $scope, invoiceFactory, utils){
+
+	invoiceFactory.getInvoice(function(data){
+		$scope.invoices = data;
+		$scope.factura = utils.findById($scope.invoices, $stateParams.facId);
+	})
+}
