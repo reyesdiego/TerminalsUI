d6c30194a516a45d92448c0a626e718ba7dbc590
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2014-04-24 12:53:25 -0300

    Adding Roles to returned data user login

--- controller/login.js
+++ controller/login.js
@@ -10,15 +10,11 @@ function loginCtrl($scope, $rootScope, userFactory, $state, $dialogs, loginServi
 				$rootScope.esTerminal = false;
 				$dialogs.error('Los datos de inicio de sesión son incorrectos');
 			} else {
-				//Por ahora solo acceso a terminales
-				var rutasAcceso = ['tarifario', 'invoices', 'invoices.result', 'invoices.search', 'matches', 'control', 'cfacturas', 'cfacturas.result', 'gates', 'gates.result', 'gates.result.container', 'gates.result.invoices', 'gates.result.invoices.result'];
-				//var rutasTerminales = ['tarifario', 'invoices', 'invoices.result', 'matches']; // El que se utiliza en producción
-				//$rootScope.esTerminal = true;
 				loginService.setInfo(data);
 				loginService.setStatus(true);
-				loginService.setType('terminal');
+				loginService.setType(data.role);
 				loginService.setToken(data.token.token);
-				loginService.setAcceso(rutasAcceso);
+				loginService.setAcceso(data.acceso);
 				$state.transitionTo('tarifario');
 
 				$rootScope.esUsuario = loginService.getType();
