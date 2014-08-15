1e13716008d14df8ce44ef8fc7133a90d7f3e4d4
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2014-08-15 16:44:39 -0300

    Agregado campo de texto en la pantalla de tarifario para realizar búsquedas según #44. Mejorado el método de paginación.

--- controller/pricelist.js
+++ controller/pricelist.js
@@ -8,11 +8,25 @@ function pricelistCtrl($scope, priceFactory, loginService){
 
 	priceFactory.getPrice(loginService.getFiltro(), function (data) {
 		$scope.pricelist = data.data;
-		$scope.filteredPrices = $scope.pricelist.slice(($scope.currentPage - 1) * $scope.itemsPerPage, $scope.currentPage * $scope.itemsPerPage);
 		$scope.totalItems = $scope.pricelist.length;
 	});
 
-	$scope.pageChanged = function(){
-		$scope.filteredPrices = $scope.pricelist.slice(($scope.currentPage - 1) * $scope.itemsPerPage, $scope.currentPage * $scope.itemsPerPage);
-	};
+	$scope.$watch('search', function(){
+		if ($scope.search != "" && $scope.search != null){
+			//Se supone que siempre que busca se limiten los resultados a una sola página, por eso seteo
+			//el totalItems en 1
+			$scope.totalItems = 1;
+			if ($scope.search.length <= 1){
+				//Una búsqueda con un solo caracter producía demasiados resultados, por lo que solo muestro los 10 primeros
+				$scope.itemsPerPage = 10;
+			} else {
+				//Si los resultados estaban originalmente en una página distinta de la currentPage no se veían,
+				//de este modo todos los resultados van hasta la única página
+				$scope.itemsPerPage = $scope.pricelist.length;
+			}
+		} else {
+			$scope.totalItems = $scope.pricelist.length;
+			$scope.itemsPerPage = 10;
+		}
+	});
 }
