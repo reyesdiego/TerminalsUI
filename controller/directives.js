168c08a289848dfc16b67d36e333633b665370be
leo <leoguty91@gmail.com>
2014-09-09 11:47:29 -0300

    #80 Actualizada las directivas para que se puedan modificar diferentes datos y reutilizar     tableInvoices, accordionComprobantesVistos, divPagination

--- controller/directives.js
+++ controller/directives.js
@@ -7,7 +7,7 @@
 			restrict:		'E',
 			templateUrl:	'view/table.invoices.html',
 			scope: {
-				invoices:			'=datos',
+				invoices:			'=datosInvoices',
 				acceso:				'@',
 				moneda:				'@',
 				filtroOrden:		'@',
@@ -30,14 +30,36 @@
 	myapp.directive('accordionComprobantesVistos', function(){
 		return {
 			restrict:		'E',
-			templateUrl:	'view/accordion.comprobantes.vistos.html'
+			templateUrl:	'view/accordion.comprobantes.vistos.html',
+			scope: {
+				comprobantesVistos:	'=datosComprobantes',
+				moneda:				'@',
+				mostrarDetalle:		'&',
+				quitarVista:		'&'
+			},
+			controller: ['$rootScope', '$scope', function($rootScope, $scope){
+				$scope.vouchersType = $rootScope.vouchersType;
+				$scope.conversionMoneda = function(importe, codMoneda, cotiMoneda){
+					if ($rootScope.moneda == 'PES' && codMoneda == 'DOL'){ return (importe * cotiMoneda);
+					} else if ($rootScope.moneda == 'DOL' && codMoneda == 'PES'){ return (importe / cotiMoneda);
+					} else { return (importe); }
+				};
+			}]
 		}
 	});
 
 	myapp.directive('divPagination', function(){
 		return {
 			restrict:		'E',
-			templateUrl:	'view/div.pagination.html'
+			templateUrl:	'view/div.pagination.html',
+			scope: {
+				currentPage:		'=',
+				totalItems:			'=',
+				pageChanged:		'&'
+			},
+			link: function($scope){
+				$scope.$watch('currentPage', function(){ $scope.pageChanged() })
+			}
 		}
 	});
 })();
