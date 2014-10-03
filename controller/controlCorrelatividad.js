d75f4952fa1b3bf1d59c2ff758f3209170a2277d
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2014-10-03 16:16:42 -0300

    Creada nueva directiva para la busqueda en el control de correlatividad, para arreglar bug comentado en #114

--- controller/controlCorrelatividad.js
+++ controller/controlCorrelatividad.js
@@ -13,64 +13,38 @@ function correlatividadCtrl($scope, invoiceFactory){
 		'nroPtoVenta': '',
 		'codTipoComprob': 0,
 		'fechaDesde': $scope.desde,
-		'fechaHasta': $scope.hasta,
-		'order': ''
+		'fechaHasta': $scope.hasta
 	};
-
-	$scope.ocultarFiltros = ['nroComprobante', 'documentoCliente', 'contenedor', 'codigo', 'razonSocial', 'estado', 'buque'];
-	$scope.model.codTipoComprob = 1;
 
 	$scope.pantalla = {
 		"tituloCorrelativo":  "Correlatividad",
 		"mensajeCorrelativo": "Seleccione punto de venta y tipo de comprobante para realizar la búsqueda",
-		"cartelCorrelativo": "panel-info",
+		"cartelCorrelativo": "info",
 		"resultadoCorrelativo": []
 	};
 
-	$scope.filtrar = function(filtro, contenido){
-		switch (filtro) {
-			case 'nroPtoVenta':
-				$scope.model.nroPtoVenta = contenido;
-				break;
-		}
+	$scope.$on('cambioFiltro', function(event, data){
+		$scope.model = data;
 		$scope.controlCorrelatividad();
-	};
-
-	$scope.traerPuntosDeVenta = function(){
-		invoiceFactory.getCashbox({}, function(data){
-			$scope.terminalSellPoints = data.data;
-		})
-	};
+	});
 
 	$scope.controlCorrelatividad = function(){
 		$scope.loadingCorrelatividad = true;
-		invoiceFactory.getCorrelative(cargaDatos(), function(dataComprob) {
+		invoiceFactory.getCorrelative($scope.model, function(dataComprob) {
 			$scope.result = dataComprob;
 			if ($scope.result.totalCount > 0){
 				$scope.pantalla.mensajeCorrelativo = "Se hallaron comprobantes faltantes: ";
-				$scope.pantalla.cartelCorrelativo = "panel-danger";
+				$scope.pantalla.cartelCorrelativo = "danger";
 				$scope.pantalla.tituloCorrelativo = "Error";
 				$scope.pantalla.resultadoCorrelativo = $scope.result.data;
 			} else {
 				$scope.pantalla.tituloCorrelativo =  "Éxito";
 				$scope.pantalla.mensajeCorrelativo = "No se hallaron comprobantes faltantes";
-				$scope.pantalla.cartelCorrelativo = "panel-success";
+				$scope.pantalla.cartelCorrelativo = "success";
 				$scope.pantalla.resultadoCorrelativo = [];
 			}
 			$scope.loadingCorrelatividad = false;
 		});
 	};
-
-	function cargaDatos(){
-		return {
-			'nroPtoVenta': $scope.model.nroPtoVenta,
-			'codTipoComprob': $scope.model.codTipoComprob,
-			'fechaDesde': $scope.model.fechaDesde,
-			'fechaHasta': $scope.model.fechaHasta,
-			'order': $scope.model.order
-		};
-	}
-
-	$scope.traerPuntosDeVenta();
 
 }
