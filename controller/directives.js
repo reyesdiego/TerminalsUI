85de899e4e20bc34464847907de31bd3cf1cf6dc
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2014-12-17 09:28:28 -0300

    Correccion menor

--- controller/directives.js
+++ controller/directives.js
@@ -792,7 +792,7 @@
 			restrict:		'E',
 			templateUrl:	'view/correlativeControlSearch.html',
 			controller: ['$rootScope', '$scope', 'invoiceFactory', function($rootScope, $scope, invoiceFactory){
-				$scope.ocultarFiltros = ['razonSocial', 'nroPtoVenta', 'nroComprobante', 'documentoCliente', 'codigo', 'estado', 'buque', 'contenedor', 'viaje'];
+				$scope.ocultarFiltros = ['razonSocial', 'nroPtoVenta', 'nroComprobante', 'documentoCliente', 'codigo', 'estado', 'buque', 'contenedor', 'viaje', 'itemsPerPage'];
 				$scope.fechaDesde = new Date();
 				$scope.fechaHasta = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
 				$scope.maxDate = new Date();
