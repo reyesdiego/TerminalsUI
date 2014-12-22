0113d5bc4900061fdce902d4ce4bc274baf674c7
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2014-12-22 13:38:11 -0300

    Arreglo en la paginacion de las tablas afip #165

--- controller/afip.js
+++ controller/afip.js
@@ -13,14 +13,19 @@
 			skip: 0,
 			limit: $scope.itemsPerPage
 		};
+		$scope.actualRegistro = '';
 
 		$scope.$on('cambioPagina', function(event, data){
 			$scope.currentPage = data;
-			$scope.cargaDatos();
+			$scope.cargaDatos($scope.actualRegistro);
 		});
 
 		$scope.cargaDatos = function(registro){
 			$scope.cargando = true;
+			if (registro != $scope.actualRegistro){
+				$scope.currentPage = 1;
+			}
+			$scope.actualRegistro = registro;
 			$scope.page.skip = (($scope.currentPage - 1) * $scope.itemsPerPage);
 			$scope.page.limit = $scope.itemsPerPage;
 			$scope.invoices = [];
