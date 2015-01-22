062ea19a4e267f2ccb1cd2319609e0090a3357f4
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-01-22 16:38:32 -0300

    Arreglos en filtrado en las vistas de tarifario y de matchprices.

--- controller/matchprices.js
+++ controller/matchprices.js
@@ -88,25 +88,6 @@
 			$scope.flagCambios = false;
 		};
 
-		$scope.$watch('search', function(){
-			if ($scope.search != "" && $scope.search != null){
-				//Se supone que siempre que busca se limiten los resultados a una sola página, por eso seteo
-				//el totalItems en 1
-				$scope.totalItems = 1;
-				if ($scope.search.length <= 1){
-					//Una búsqueda con un solo caracter producía demasiados resultados, por lo que solo muestro los 10 primeros
-					$scope.itemsPerPage = 10;
-				} else {
-					//Si los resultados estaban originalmente en una página distinta de la currentPage no se veían,
-					//de este modo todos los resultados van hasta la única página
-					$scope.itemsPerPage = $scope.pricelist.length;
-				}
-			} else {
-				$scope.totalItems = $scope.listaSeleccionada.length;
-				$scope.itemsPerPage = 10;
-			}
-		});
-
 		$scope.$watch('conMatch', function(){
 			if ($scope.conMatch){
 				$scope.listaSeleccionada = $scope.codigosConMatch;
