0cbcedde199bc8d1e9ec70173b22648f4c62aafa
reyesdiego@hotmail.com <reyesdiego@hotmail.com>
2015-02-04 12:00:32 -0300

    Se agrega checkeo para ver si el usuario tiene token generado para el login. Sino tiene no pasa

--- factory/auth.js
+++ factory/auth.js
@@ -25,8 +25,9 @@ myapp.factory('authFactory', function($state, $cookies, $cookieStore, userFactor
 		pass = pass || $cookies.password;
 
 		userFactory.login(user, pass, function(data, error){
+			data = data.data;
+
 			if (!error && typeof data.token === 'object') {
-				data = data.data;
 				loginService.setInfo(data);
 				loginService.setStatus(true);
 				loginService.setType(data.role);
