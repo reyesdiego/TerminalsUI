55085040c67fe5f4f65b3b70ee2af8b7c1593a64
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-03-13 13:11:27 -0300

    Cambios en archivos para compilacion #246

--- factory/cache.js
+++ factory/cache.js
@@ -1,115 +1,114 @@
 /**
  * Created by leo on 12/03/15.
  */
-(function () {
-	myapp.factory('cacheFactory', function (DSCacheFactory, controlPanelFactory, invoiceFactory, vouchersFactory, priceFactory, statesFactory, buqueViajeCache, clientesCache, contenedoresCache, contenedoresGatesCache, contenedoresTurnosCache, descripcionesCache, vouchersCache, vouchersArrayCache, unitTypesCache, unitTypesArrayCache, estadosComprobantesCache, estadosComprobantesArrayCache) {
-		var factory = {};
 
-		factory.cargaCache = function () {
-			// Buque viaje cache
-			invoiceFactory.getShipTrips(function (data) {
-				if (data.status == 'OK') {
-					buqueViajeCache.put('buqueViaje', data.data);
-					//console.log(buqueViajeCache.get('buqueViaje'));
-				}
-			});
+myapp.factory('cacheFactory', function (DSCacheFactory, controlPanelFactory, invoiceFactory, vouchersFactory, priceFactory, statesFactory, buqueViajeCache, clientesCache, contenedoresCache, contenedoresGatesCache, contenedoresTurnosCache, descripcionesCache, vouchersCache, vouchersArrayCache, unitTypesCache, unitTypesArrayCache, estadosComprobantesCache, estadosComprobantesArrayCache) {
+	var factory = {};
 
-			// Clientes cache
-			controlPanelFactory.getClients(function (data) {
-				if (data.status == 'OK') {
-					clientesCache.put('clientes', data.data);
-					//console.log(clientesCache.get('clientes'));
-				}
-			});
+	factory.cargaCache = function () {
+		// Buque viaje cache
+		invoiceFactory.getShipTrips(function (data) {
+			if (data.status == 'OK') {
+				buqueViajeCache.put('buqueViaje', data.data);
+				//console.log(buqueViajeCache.get('buqueViaje'));
+			}
+		});
 
-			// Contenedores cache
-			controlPanelFactory.getContainers(function (data) {
-				if (data.status == 'OK') {
-					contenedoresCache.put('contenedores', data.data);
-					//console.log(contenedoresCache.get('contenedores'));
-				}
-			});
+		// Clientes cache
+		controlPanelFactory.getClients(function (data) {
+			if (data.status == 'OK') {
+				clientesCache.put('clientes', data.data);
+				//console.log(clientesCache.get('clientes'));
+			}
+		});
 
-			// Contenedores gates cache
-			controlPanelFactory.getContainersGates(function (data) {
-				if (data.status == 'OK') {
-					contenedoresGatesCache.put('contenedoresGates', data.data);
-					//console.log(contenedoresGatesCache.get('contenedoresGates'));
-				}
-			});
+		// Contenedores cache
+		controlPanelFactory.getContainers(function (data) {
+			if (data.status == 'OK') {
+				contenedoresCache.put('contenedores', data.data);
+				//console.log(contenedoresCache.get('contenedores'));
+			}
+		});
 
-			// Contenedores turnos cache
-			controlPanelFactory.getContainersTurnos(function (data) {
-				if (data.status == 'OK') {
-					contenedoresTurnosCache.put('contenedoresTurnos', data.data);
-					//console.log(contenedoresTurnosCache.get('contenedoresTurnos'));
-				}
-			});
+		// Contenedores gates cache
+		controlPanelFactory.getContainersGates(function (data) {
+			if (data.status == 'OK') {
+				contenedoresGatesCache.put('contenedoresGates', data.data);
+				//console.log(contenedoresGatesCache.get('contenedoresGates'));
+			}
+		});
 
-			// Descripciones cache
-			invoiceFactory.getDescriptionItem(function (data) {
-				if (data.status == 'OK') {
-					descripcionesCache.put('descripciones', data.data);
-				}
-			});
+		// Contenedores turnos cache
+		controlPanelFactory.getContainersTurnos(function (data) {
+			if (data.status == 'OK') {
+				contenedoresTurnosCache.put('contenedoresTurnos', data.data);
+				//console.log(contenedoresTurnosCache.get('contenedoresTurnos'));
+			}
+		});
 
-			// Vouchers cache
-			vouchersFactory.getVouchersType(function (data) {
-				if (data.status == 'OK') {
-					vouchersCache.put('vouchers', data.data);
-					data.data.forEach(function (dato) {
-						vouchersArrayCache.put(dato._id, dato.description);
-					});
-				}
-			});
+		// Descripciones cache
+		invoiceFactory.getDescriptionItem(function (data) {
+			if (data.status == 'OK') {
+				descripcionesCache.put('descripciones', data.data);
+			}
+		});
 
-			// Unit Type cache
-			priceFactory.getUnitTypes(function (data) {
-				if (data.status == 'OK') {
-					unitTypesCache.put('unitTypes', data.data);
-					data.data.forEach(function (dato) {
-						unitTypesArrayCache.put(dato._id, dato.description);
-					});
-				}
-			});
+		// Vouchers cache
+		vouchersFactory.getVouchersType(function (data) {
+			if (data.status == 'OK') {
+				vouchersCache.put('vouchers', data.data);
+				data.data.forEach(function (dato) {
+					vouchersArrayCache.put(dato._id, dato.description);
+				});
+			}
+		});
 
-			// States cache
-			statesFactory.getStatesType(function (data) {
-				if (data.status == 'OK') {
-					var estados = data.data;
-					estados.forEach(function (estado) {
-						switch (estado.type) {
-							case 'WARN':
-								estado.icon = '<img src="images/warn.png" />';
-								estado.imagen = 'images/warn.png';
-								break;
-							case 'ERROR':
-								estado.icon = '<img src="images/error.png" />';
-								estado.imagen = 'images/error.png';
-								break;
-							case 'UNKNOWN':
-								estado.icon = '<img src="images/unknown.png" />';
-								estado.imagen = 'images/unknown.png';
-								break;
-							case 'OK':
-								estado.icon = '<img src="images/ok.png" />';
-								estado.imagen = 'images/ok.png';
-								break;
-						}
-					});
-					estadosComprobantesCache.put('estadosComprobantes', estados);
-					data.data.forEach(function (dato) {
-						estadosComprobantesArrayCache.put(dato._id, dato);
-					});
-					//console.log(estadosComprobantesCache.get('Y'));
-				}
-			});
-		};
+		// Unit Type cache
+		priceFactory.getUnitTypes(function (data) {
+			if (data.status == 'OK') {
+				unitTypesCache.put('unitTypes', data.data);
+				data.data.forEach(function (dato) {
+					unitTypesArrayCache.put(dato._id, dato.description);
+				});
+			}
+		});
 
-		factory.borraCache = function () {
-			DSCacheFactory.clearAll();
-		};
+		// States cache
+		statesFactory.getStatesType(function (data) {
+			if (data.status == 'OK') {
+				var estados = data.data;
+				estados.forEach(function (estado) {
+					switch (estado.type) {
+						case 'WARN':
+							estado.icon = '<img src="images/warn.png" />';
+							estado.imagen = 'images/warn.png';
+							break;
+						case 'ERROR':
+							estado.icon = '<img src="images/error.png" />';
+							estado.imagen = 'images/error.png';
+							break;
+						case 'UNKNOWN':
+							estado.icon = '<img src="images/unknown.png" />';
+							estado.imagen = 'images/unknown.png';
+							break;
+						case 'OK':
+							estado.icon = '<img src="images/ok.png" />';
+							estado.imagen = 'images/ok.png';
+							break;
+					}
+				});
+				estadosComprobantesCache.put('estadosComprobantes', estados);
+				data.data.forEach(function (dato) {
+					estadosComprobantesArrayCache.put(dato._id, dato);
+				});
+				//console.log(estadosComprobantesCache.get('Y'));
+			}
+		});
+	};
 
-		return factory;
-	});
-})();
+	factory.borraCache = function () {
+		DSCacheFactory.clearAll();
+	};
+
+	return factory;
+});
