55de2063aebe8f57fdb63921712d8d01c20ac29f
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-03-13 13:10:08 -0300

    Cambios en archivos para compilacion #246

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,11 +12,12 @@ module.exports = function(grunt) {
 			},
 			js : {
 				src : [
+					'main/*',
+					'includes/js/utils/configProd.js',
 					'controller/*',
 					'directives/*',
 					'factory/*',
 					'filter/*',
-					'includes/js/utils/configProd.js',
 					'service/*'
 				],
 				dest : 'build/js/app.js'
@@ -34,6 +35,13 @@ module.exports = function(grunt) {
 					'build/js/app-min.js' : ['build/js/app.js']
 				}
 			}
+		},
+		processhtml: {
+			dist: {
+				files: {
+					'build/index.html' : 'index.html'
+				}
+			}
 		}
 	});
 
@@ -41,7 +49,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-processhtml');
 
 	// Default task(s).
-	grunt.registerTask('default', ['concat:css','concat:js', 'cssmin:css', 'uglify:js']);
+	grunt.registerTask('default', ['concat:css','concat:js', 'cssmin:css', 'uglify:js', 'processhtml:dist']);
 };
