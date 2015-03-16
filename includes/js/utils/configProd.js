f5141472e790ae473723c311116e02cbbe77aa04
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-03-16 16:47:33 -0300

    cambio provisorio

--- includes/js/utils/configProd.js
+++ includes/js/utils/configProd.js
@@ -3,6 +3,7 @@
  */
 var config = {
 	url : function(){
-		return "http://200.123.104.182:8081"; // Servidor Desarrollo (externo)
+		//return "http://200.123.104.182:8081"; // Servidor Desarrollo (externo);
+		return "http://10.10.0.223:8080"; // Diego - Provisional
 	}
 };
