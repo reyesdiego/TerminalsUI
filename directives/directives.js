5f2d9844a5f809f02ea0c06a47cfd0233bc74a11
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-03-30 11:29:37 -0300

    Cambio gif de carga. Comentada linea de carga de buques en cache, para poder probar en servidor sin que arroje error

--- directives/directives.js
+++ directives/directives.js
@@ -1205,7 +1205,7 @@ myapp.directive('divCargando', [function () {
 		},
 		template:
 			'<div class="col-lg-12 text-center" ng-show="mostrar">' +
-			'	<img class="media-object" style="margin-left: 40%; margin-top: 50px" ng-src="images/ajax_loader_gray_512.gif" width="252" height="252">' +
+			'	<img class="media-object" style="margin-left: 40%; margin-top: 50px" ng-src="images/grua_carga.gif" width="252" height="252">' +
 			'</div>' +
 			'<div class="col-lg-12" ng-hide="mostrar">' +
 			'	<div ng-transclude></div>' +
