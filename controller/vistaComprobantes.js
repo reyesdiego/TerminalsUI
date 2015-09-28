031252d26f917d5a311b2b6afca848fe0614ca41
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-09-28 12:11:27 -0300

    Depuracion de codigo

--- controller/vistaComprobantes.js
+++ controller/vistaComprobantes.js
@@ -1,8 +1,8 @@
 /**
  * Created by artiom on 30/03/15.
  */
-myapp.controller('vistaComprobantesCtrl', ['$rootScope', '$scope', '$filter', 'invoiceFactory', 'loginService', 'statesFactory', 'generalCache', 'generalFunctions', 'dialogs', 'invoiceService',
-	function($rootScope, $scope, $filter, invoiceFactory, loginService, statesFactory, generalCache, generalFunctions, dialogs, invoiceService){
+myapp.controller('vistaComprobantesCtrl', ['$rootScope', '$scope', 'invoiceFactory', 'loginService', 'generalFunctions', 'dialogs', 'invoiceService',
+	function($rootScope, $scope, invoiceFactory, loginService, generalFunctions, dialogs, invoiceService){
 		$scope.status = {
 			open: true
 		};
@@ -11,22 +11,7 @@ myapp.controller('vistaComprobantesCtrl', ['$rootScope', '$scope', '$filter', 'i
 		//Variables para control de fechas
 		$scope.maxDateD = new Date();
 		$scope.maxDateH = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-		//Tipos de comprobantes
-		//$scope.vouchers = generalCache.get('vouchers');
-		//Listas para autocompletado
-		//$scope.listaViajes = [];
-		/*$scope.itemsPerPage = [
-			{ value: 10, description: '10 items por página', ticked: false},
-			{ value: 15, description: '15 items por página', ticked: true},
-			{ value: 20, description: '20 items por página', ticked: false},
-			{ value: 50, description: '50 items por página', ticked: false}
-		];*/
-		//$scope.estadosComprobantes = $filter('filter')(generalCache.get('estados'), $scope.filtroEstados);
 		$scope.logoTerminal = $rootScope.logoTerminal;
-
-		/*$scope.estadosComprobantes.forEach(function(unEstado){
-			unEstado.ticked = false;
-		});*/
 
 		$scope.comprobantesVistos = [];
 
@@ -82,10 +67,6 @@ myapp.controller('vistaComprobantesCtrl', ['$rootScope', '$scope', '$filter', 'i
 			$scope.mostrarResultado = false;
 		});
 
-		/*$scope.cambioItemsPorPagina = function(data){
-			$scope.filtrado('itemsPerPage', data.value);
-		};*/
-
 		$scope.$watch('model.rates', function(){
 			if ($scope.model.rates != 1) $scope.model.payment = '';
 		});
@@ -93,77 +74,6 @@ myapp.controller('vistaComprobantesCtrl', ['$rootScope', '$scope', '$filter', 'i
 		$scope.$watch('model.payment', function(){
 			if ($scope.model.payment != 1) $scope.model.payed = '';
 		});
-
-		/*$scope.estadoSeleccionado = function(data){
-			var contenido = '';
-			if (data.ticked){
-				$scope.estadosComprobantes.forEach(function(unEstado){
-					if (unEstado.ticked){
-						if (contenido == ''){
-							contenido += unEstado._id;
-						} else {
-							contenido += ',' + unEstado._id;
-						}
-					}
-				});
-			} else {
-				contenido = $scope.model.estado.replace(',' + data._id, '');
-				contenido = contenido.replace(data._id + ',', '');
-				contenido = contenido.replace(data._id, '');
-				if (contenido == ''){
-					contenido = 'N';
-				}
-			}
-			$scope.filtrado('estado', contenido);
-		};*/
-
-		/*$scope.hitEnter = function(evt){
-			if(angular.equals(evt.keyCode,13))
-				$scope.cargaPuntosDeVenta();
-		};*/
-
-		/*$scope.openDate = function(event){
-			generalFunctions.openDate(event);
-		};*/
-
-		/*$scope.clientSelected = function(selected){
-			if (angular.isDefined(selected) && selected.title != $scope.model.razonSocial){
-				$scope.model.razonSocial = selected.title;
-				$scope.filtrado('razonSocial', selected.title);
-			}
-		};*/
-
-		/*$scope.buqueSelected = function(selected){
-			if (angular.isDefined(selected) && selected.title != $scope.model.buqueNombre){
-				$scope.model.buqueNombre = selected.originalObject.buque;
-				var i = 0;
-				selected.originalObject.viajes.forEach(function(viaje){
-					var objetoViaje = {
-						'id': i,
-						'viaje': viaje.viaje
-					};
-					$scope.listaViajes.push(objetoViaje);
-					i++;
-				});
-			}
-		};*/
-
-		/*$scope.viajeSelected = function(selected){
-			if (angular.isDefined(selected) && selected.title != $scope.model.viaje){
-				$scope.model.viaje = selected.title;
-				$scope.filtrado('viaje', selected.title);
-			}
-		};*/
-
-		/*$scope.aBuscar = function(){
-			$scope.loadingState = true;
-			$scope.mostrarResultado = false;
-			$scope.currentPage = 1;
-			for (var elemento in $scope.model){
-				if (!angular.isDefined($scope.model[elemento])) $scope.model[elemento] = '';
-			}
-			$scope.cargaPuntosDeVenta();
-		};*/
 
 		$scope.filtrado = function(filtro, contenido){
 			$scope.loadingState = true;
@@ -186,22 +96,6 @@ myapp.controller('vistaComprobantesCtrl', ['$rootScope', '$scope', '$filter', 'i
 				$scope.cargaPuntosDeVenta();
 			}
 		};
-
-		/*$scope.filtrarCaracteresInvalidos = function(palabra){
-			if (angular.isDefined(palabra) && palabra.length > 0){
-				var palabraFiltrada;
-				var caracteresInvalidos = ['*', '(', ')', '+', ':', '?'];
-				palabraFiltrada = palabra;
-				for (var i = 0; i <= caracteresInvalidos.length - 1; i++){
-					if (palabraFiltrada.indexOf(caracteresInvalidos[i], 0) > 0){
-						palabraFiltrada = palabraFiltrada.substring(0, palabraFiltrada.indexOf(caracteresInvalidos[i], 0));
-					}
-				}
-				return palabraFiltrada.toUpperCase();
-			} else {
-				return palabra;
-			}
-		};*/
 
 		$scope.filtrarOrden = function(filtro){
 			$scope.currentPage = 1;
