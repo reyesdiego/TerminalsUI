fb024b7d85f563f935436e9681fa6f6c9ea9c8f8
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-10-19 11:40:58 -0300

    Mejoras en interfaz de reporte de empresas y agregado manejo de errores #325

--- controller/reporteEmpresas.js
+++ controller/reporteEmpresas.js
@@ -4,10 +4,24 @@
 myapp.controller('facturacionPorEmpresaCtrl', ['$scope', 'controlPanelFactory', 'dialogs', 'loginService',
 	function($scope, controlPanelFactory, dialogs, loginService){
 
+		$scope.cargando = false;
+
 		$scope.fechaInicio = new Date();
 		$scope.fechaFin = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
 
 		$scope.ocultarFiltros = ['nroPtoVenta', 'nroComprobante', 'codTipoComprob', 'documentoCliente', 'contenedor', 'codigo', 'estado', 'buque', 'viaje', 'itemsPerPage'];
+
+		$scope.mensajeResultado = {
+			titulo: 'Reporte empresas',
+			mensaje: 'Seleccione una razón social para realizar la búsqueda.',
+			tipo: 'panel-info'
+		};
+
+		$scope.$on('errorInesperado', function(e, mensaje){
+			$scope.cargando = false;
+			$scope.invoices = [];
+			$scope.mensajeResultado = mensaje;
+		});
 
 		$scope.model = {
 			fechaInicio: $scope.fechaInicio,
@@ -29,16 +43,25 @@ myapp.controller('facturacionPorEmpresaCtrl', ['$scope', 'controlPanelFactory', 
 		$scope.todasLasTerminales = true;
 
 		$scope.cargarReporte = function(){
+			$scope.cargando = true;
 			if (!$scope.todasLasTerminales){
 				$scope.model.terminal = loginService.getFiltro();
 			} else {
 				$scope.model.terminal = ''
 			}
 			controlPanelFactory.getFacturacionEmpresas($scope.model, function(data){
-				console.log(data);
 				if (data.status == 'OK'){
 					$scope.resultados = data.data;
+					$scope.mensajeResultado.mensaje = 'No se hallaron datos de facturación para ' + $scope.model.razonSocial + ' entre las fechas seleccionadas.';
+				} else {
+					$scope.mensajeResultado = {
+						titulo: 'Error',
+						mensaje: 'Se produjo un error al cargar los datos. Inténtelo nuevamente más tarde o comuníquese con el soporte técnico.',
+						tipo: 'panel-danger'
+					};
+					$scope.resultados = [];
 				}
+				$scope.cargando = false;
 			})
 		}
 }]);
