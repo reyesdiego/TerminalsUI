efc3a5dddb0c8b4e8471bd2632f695860d7c951b
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2015-10-29 14:24:57 -0300

    Cambios en directiva de graficos, agregando mayor funcionalidad al seleccionar una parte del grafico, y exportando su imagen a JPG para poder imprimirla en reportes PDF #342

--- directives/chartDirectives.js
+++ directives/chartDirectives.js
@@ -7,7 +7,7 @@ myapp.directive('dynamicChart', ['$timeout', function($timeout){
 		restrict: 'E',
 		scope: {
 			chartObject:	'=',
-			selectFn:		'&',
+			selectFn:		'&select',
 			series:			'=',
 			colors:			'='
 		},
@@ -16,6 +16,27 @@ myapp.directive('dynamicChart', ['$timeout', function($timeout){
 			var data; //= new google.visualization.arrayToDataTable($scope.data);
 			var chart;
 			var prefijo;
+
+			var options = {
+				'title': $scope.chartObject.title,
+				'width': $scope.chartObject.width,
+				'height': $scope.chartObject.height,
+				'series': $scope.chartObject.series,
+				'backgroundColor': {'fill': 'transparent'},
+				'animation':{
+					duration: 1000,
+					easing: 'out'
+				},
+				'legend': { position: 'top', maxLines: 3 },
+				'bar': { groupWidth: '75%' },
+				'is3D': $scope.chartObject.is3D,
+				'isStacked': $scope.chartObject.stacked,
+				'slices': {}
+			};
+
+			var selectedSlice = -1;
+			var controlSelect;
+			var selectItem = false;
 
 			switch ($scope.chartObject.type){
 				case 'column':
@@ -32,7 +53,7 @@ myapp.directive('dynamicChart', ['$timeout', function($timeout){
 			draw();
 
 			// Watches, to refresh the chart when its data, title or dimensions change
-			$scope.$watch('chartObject', function() {
+			$scope.$watch('chartObject.data', function() {
 				draw();
 			}, true); // true is for deep object equality checking
 			/*$scope.$watch('title', function() {
@@ -47,19 +68,65 @@ myapp.directive('dynamicChart', ['$timeout', function($timeout){
 
 			// Chart selection handler
 			google.visualization.events.addListener(chart, 'select', function () {
+				controlSelect = chart.getSelection();
 				var selectedItem = chart.getSelection()[0];
 				if (selectedItem) {
+					if ($scope.chartObject.type == 'pie'){
+						var rowNumber = parseInt(selectedItem.row);
+
+						if(selectedSlice != -1){    // If we have a selection, unexplode it
+							options.slices[selectedSlice] = {offset:'0'};
+						}
+						if(selectedSlice == rowNumber){ // If this is already selected, unselect it
+							selectedSlice = -1;
+						}else{  // else explode it
+							options.slices[rowNumber] = {offset:'.2'};
+							selectedSlice = rowNumber;
+							selectItem = true;
+						}
+
+						chart.draw(data, options);
+					}
 					$scope.$apply(function () {
 						$scope.selectFn({selectedRowIndex: selectedItem.row});
 					});
+				} else {
+					if ($scope.chartObject.type == 'pie'){
+						options.slices[selectedSlice] = {offset:'0'};
+						selectedSlice = -1
+						chart.draw(data, options);
+					}
 				}
 			});
 
 			google.visualization.events.addListener(chart, 'ready', function () {
 				$scope.$apply(function(){
-					$scope.chartObject.image = chart.getImageURI();
+					if (selectItem){
+						chart.setSelection(controlSelect);
+						selectItem = false;
+					}
+					convertirPNGaJPG(chart.getImageURI(), function(jpgImg){
+						$scope.chartObject.image = jpgImg;
+					});
 				});
 			});
+
+			function convertirPNGaJPG(base64Png, callback){
+				var img = new Image();
+				img.onload = function(){
+					var canvas = document.createElement('CANVAS'),
+						ctx = canvas.getContext('2d'), dataURL;
+					canvas.height = this.height;
+					canvas.width = this.width;
+					ctx.fillStyle = 'white';
+					ctx.fillRect(0, 0, canvas.width, canvas.height);
+					ctx.drawImage(this, 0, 0);
+					dataURL = canvas.toDataURL('image/jpeg');
+					callback(dataURL);
+					canvas = null;
+				};
+				img.src = base64Png;
+			}
 
 			function draw() {
 				if (!draw.triggered) {
@@ -75,6 +142,7 @@ myapp.directive('dynamicChart', ['$timeout', function($timeout){
 								break;
 						}
 						if ($scope.chartObject.type == 'pie'){
+							options.tooltip = {trigger: 'selection'};
 							var label, value;
 							data.removeRows(0, data.getNumberOfRows());
 							angular.forEach($scope.chartObject.data, function(row) {
@@ -85,21 +153,6 @@ myapp.directive('dynamicChart', ['$timeout', function($timeout){
 								}
 							});
 						}
-						var options = {
-							'title': $scope.chartObject.title,
-							'width': $scope.chartObject.width,
-							'height': $scope.chartObject.height,
-							'series': $scope.chartObject.series,
-							'backgroundColor': {'fill': 'transparent'},
-							'animation':{
-								duration: 1000,
-								easing: 'out'
-							},
-							'legend': { position: 'top', maxLines: 3 },
-							'bar': { groupWidth: '75%' },
-							'is3D': $scope.chartObject.is3D,
-							'isStacked': $scope.chartObject.stacked
-						};
 						if (!angular.equals($scope.colors, undefined)){
 							options.colors = [$scope.colors.bactssa, $scope.colors.terminal4, $scope.colors.trp, 'green'];
 						}
