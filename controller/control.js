4ab952ea31c6ddaea237adb40ade149463af2196
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-03-14 15:27:41 -0300

    Cambios en gráfico de facturado diario

--- controller/control.js
+++ controller/control.js
@@ -252,7 +252,7 @@ myapp.controller('controlCtrl', ['$rootScope', '$scope', 'controlPanelFactory', 
 			datosGrafico.forEach(function(datosDia){
 				if (flagPrimero){
 					//Primera iteración, cargo el día y lo establezco como fecha para comparar
-					fila[0] = datosDia.date.substr(0, 10).replace('-', '/');
+					fila[0] = datosDia.date.substr(0, 10).replace(/-/g, '/');
 					fechaAnterior = datosDia.date;
 					//fila[0] = datosDia._id.day + '/' + datosDia._id.month + '/' + datosDia._id.year;
 					//fechaAnterior = datosDia._id.day;
@@ -264,7 +264,7 @@ myapp.controller('controlCtrl', ['$rootScope', '$scope', 'controlPanelFactory', 
 					base.push(fila.slice());
 					//Meto la fila en la matriz y vuelvo a empezar
 					fila = ['', 0, 0, 0, 0, ''];
-					fila[0] = datosDia.date.substr(0, 10).replace('-', '/');
+					fila[0] = datosDia.date.substr(0, 10).replace(/-/g, '/');
 					fechaAnterior = datosDia.date;
 					acum = 0;
 				}
