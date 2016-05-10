b78720c1dd8622eb1af6a4a8fa32cc0c5180f511
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-05-10 17:17:51 -0300

    Finalizado reporte de tarifas por terminal

--- controller/tarifasTerminales.js
+++ controller/tarifasTerminales.js
@@ -2,6 +2,57 @@
  * Created by kolesnikov-a on 10/05/2016.
  */
 
-myapp.controller ('tarifasTerminalesCtrl', ['$scope', function($scope){
+myapp.controller ('tarifasTerminalesCtrl', ['$scope', 'reportsFactory', 'loginService', 'generalCache', function($scope, reportsFactory, loginService, generalCache){
+
+    $scope.descripciones = generalCache.get('descripciones' + loginService.getFiltro());
+
+    $scope.model = {
+        tipo: 'year',
+        fecha: new Date(),
+        loading: false
+    };
+
+    $scope.mensajeResultado = {
+        titulo: 'Reporte de tarifas',
+        mensaje: 'Seleccione año o mes y año y presione cargar para ver el reporte.',
+        tipo: 'panel-info'
+    };
+
+    $scope.dataReporte = [];
+
+    $scope.search = '';
+
+    $scope.cargarReporte = function(){
+        $scope.model.loading = true;
+        var param = {
+            year: $scope.model.fecha.getFullYear()
+        };
+        if ($scope.model.tipo == 'month'){
+            param.month = $scope.model.fecha.getMonth() + 1;
+        }
+        reportsFactory.getReporteTerminales(param, function(data){
+            $scope.model.loading = false;
+            if (data.status == 'OK'){
+                $scope.dataReporte = data.data;
+                $scope.dataReporte.forEach(function(tarifa){
+                    tarifa.descripcion = angular.isDefined($scope.descripciones[tarifa.code]) ? $scope.descripciones[tarifa.code] : 'Sin definir'
+                })
+            } else {
+                $scope.dataReporte = [];
+                $scope.mensajeResultado = {
+                    titulo: 'Reporte de tarifas',
+                    mensaje: 'Seleccione año o mes y año y presione cargar para ver el reporte.',
+                    tipo: 'panel-danger'
+                };
+            }
+        });
+    };
+
+    $scope.$on('cambioTerminal', function(){
+        $scope.descripciones = generalCache.get('descripciones' + loginService.getFiltro());
+        if ($scope.dataReporte.length > 0){
+            $scope.cargarReporte();
+        }
+    })
 
 }]);
--- factory/reports.js
+++ factory/reports.js
@@ -2,8 +2,18 @@
  * Created by Artiom on 17/06/14.
  */
 
-myapp.factory('reportsFactory', ['$http', 'dialogs', 'formatService', function($http, dialogs, formatService){
+myapp.factory('reportsFactory', ['$http', 'dialogs', 'formatService', 'loginService', function($http, dialogs, formatService, loginService){
 	var factory = {};
+
+	factory.getReporteTerminales = function(param, callback){
+		var inserturl = serverUrl + '/invoices/' + loginService.getFiltro() + '/byRates';
+		$http.get(inserturl, {params: formatService.formatearDatos(param)})
+				.success(function(response){
+					callback(response);
+				}).error(function(error){
+					callback(error);
+				});
+	};
 
 	factory.getReporteTarifas = function(fecha, tarifas, callback){
 		var inserturl = serverUrl + '/invoices/byRates?fechaInicio=' + formatService.formatearFechaISOString(fecha.fechaInicio) + '&fechaFin=' + formatService.formatearFechaISOString(fecha.fechaFin);
