9a2cc3e879b9bb421b29face971ee3bd195c0dc4
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-08-25 11:03:02 -0300

    Corrección en matchprices

--- class/price.js
+++ class/price.js
@@ -158,8 +158,9 @@ myapp.factory('Price', ['$http', 'unitTypesArrayCache', '$q', 'formatService', '
         saveMatchPrices: function(){
             var deferred = $q.defer();
             var inserturl = serverUrl + '/matchPrices/matchprice';
-            $http.post(inserturl, this.matches[0])
+            $http.post(inserturl, this.matches)
                 .then(function (response) {
+                    console.log(response);
                     deferred.resolve(response.data);
                 }, function(response) {
                     deferred.reject(response.data);
