502b8f22590b3f84a038fc995896c80ef1b2b3a8
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-09-20 16:06:43 -0300

    Arreglos en interfaz y funcionamiento en matchprices

--- class/price.js
+++ class/price.js
@@ -172,7 +172,7 @@ myapp.factory('Price', ['$http', 'unitTypesArrayCache', '$q', 'formatService', '
         saveMatchPrices: function(){
             var deferred = $q.defer();
             var inserturl = APP_CONFIG.SERVER_URL + '/matchPrices/matchprice';
-            $http.post(inserturl, this.matches)
+            $http.post(inserturl, this.matches[0])
                 .then(function (response) {
                     deferred.resolve(response.data);
                 }, function(response) {
