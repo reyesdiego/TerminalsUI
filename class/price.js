e45c7a6aa51ce7a2ca688cd989adf3b5155e1847
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-10-12 11:34:14 -0300

    Cambios para que todo funcione con la versión actual del servidor en producción

--- class/price.js
+++ class/price.js
@@ -31,7 +31,21 @@ myapp.factory('Price', ['$http', 'unitTypesArrayCache', '$q', 'formatService', '
             this.tarifaAgp = false;
             this.servicio = false;
 
-            if (!angular.isDefined(this.price || this.price == null)){
+            //********* El server todavia no está actualizado en produccion *********************
+            if (!angular.isDefined(this.topPrices[0].price || this.topPrices[0].price == null)){
+                this.orderPrice = 0;
+            } else {
+                this.orderPrice = this.topPrices[0].price;
+                this.orderCurrency = this.topPrices[0].currency;
+            }
+            this.nuevoTopPrice = {
+                currency: this.topPrices[0].currency,
+                price: this.orderPrice,
+                from: new Date()
+            };
+            //********** Esto se va cuando lo actualicen y queda lo de abajo ***********************
+
+            /*if (!angular.isDefined(this.price || this.price == null)){
                 this.orderPrice = 0;
             } else {
                 this.orderPrice = this.price;
@@ -41,13 +55,14 @@ myapp.factory('Price', ['$http', 'unitTypesArrayCache', '$q', 'formatService', '
                 currency: this.currency,
                 price: this.orderPrice,
                 from: new Date()
-            };
+            };*/
             if (this.tieneMatch()){
                 this.matches[0]._idPrice = this._id;
             } else {
                 this.matches = [{
                     terminal: loginService.getInfo().terminal,
-                    match: [],
+                    match: [this.code], //TODO: guarda con esto
+                    //match: [],
                     _idPrice: this._id,
                     code: this.code
                 }];
@@ -172,7 +187,8 @@ myapp.factory('Price', ['$http', 'unitTypesArrayCache', '$q', 'formatService', '
         saveMatchPrices: function(){
             var deferred = $q.defer();
             var inserturl = APP_CONFIG.SERVER_URL + '/matchPrices/matchprice';
-            $http.post(inserturl, this.matches[0])
+            //$http.post(inserturl, this.matches[0]) -------------- proxima actualizacion
+            $http.post(inserturl, this.matches)
                 .then(function (response) {
                     deferred.resolve(response.data);
                 }, function(response) {
