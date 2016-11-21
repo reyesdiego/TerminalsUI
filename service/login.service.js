9e7c8ab1b3bfa541f281ad9ec7eb1d15a58b419f
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2016-11-21 11:14:01 -0300

    Otorgada mayor responsabilidad al loginService como proveedor de la app entera

--- service/login.service.js
+++ service/login.service.js
@@ -2,52 +2,116 @@
  * Created by diego on 3/26/14.
  */
 
-myapp.service('loginService', [function () {
-	return {
-		getStatus: function () {
-			return sessionStorage.getItem('loginStatus');
-		},
-		setStatus: function (value) {
-			sessionStorage.setItem('loginStatus', value);
-		},
-		getToken: function () {
-			return sessionStorage.getItem('token');
-		},
-		setToken: function (value) {
-			sessionStorage.setItem('token', value);
-		},
-		setType: function (value) {
-			sessionStorage.setItem('type', value);
-		},
-		getType: function () {
+myapp.service('loginService', ['generalFunctions', '$cookies', '$http', function (generalFunctions, $cookies, $http) {
+
+	this.logoTerminal = 'images/logo_bactssa.png';
+	this.terminalEstilo = 'bootstrap.cerulean';
+
+	this.getStatus = function () {
+		return sessionStorage.getItem('loginStatus');
+	};
+
+	this.setStatus = function (value) {
+		sessionStorage.setItem('loginStatus', value);
+	};
+
+	this.getToken = function () {
+		return sessionStorage.getItem('token');
+	};
+
+	this.setToken = function (value) {
+		sessionStorage.setItem('token', value);
+	};
+
+	this.setType = function (value) {
+		sessionStorage.setItem('type', value);
+	};
+
+	this.getType = function () {
+		if (sessionStorage.getItem('type')){
 			return sessionStorage.getItem('type');
-		},
-		setGroup: function (value) {
-			sessionStorage.setItem('group', value);
-		},
-		getGroup: function () {
-			return sessionStorage.getItem('group');
-		},
-		setAcceso: function(value) {
-			sessionStorage.acceso = JSON.stringify(value);
-		},
-		getAcceso: function() {
+		} else {
+			return '';
+		}
+
+	};
+
+	this.setGroup = function (value) {
+		sessionStorage.setItem('group', value);
+	};
+
+	this.getGroup = function () {
+		return sessionStorage.getItem('group');
+	};
+
+	this.setAcceso = function(rutas) {
+
+		if (generalFunctions.in_array('cfacturas', rutas)){
+			rutas = [...rutas, ...['cfacturas.tasas', 'cfacturas.correlatividad', 'cfacturas.codigos', 'cfacturas.revisar', 'cfacturas.erroneos', 'cfacturas.reenviar']];
+		}
+		if (generalFunctions.in_array('reports', rutas)){
+			rutas = [...rutas, ...['reports.tasas', 'reports.tarifas', 'reports.empresas', 'reports.terminales']];
+		}
+		if (generalFunctions.in_array('cgates', rutas)){
+			rutas = [...rutas, ...['cgates.gates', 'cgates.invoices', 'cgates.appointments']];
+		}
+		rutas.push('trackContainer');
+
+		sessionStorage.acceso = JSON.stringify(rutas);
+	};
+
+	this.getAcceso = function() {
+		if (sessionStorage.acceso){
 			return JSON.parse(sessionStorage.acceso);
-		},
-		setInfo: function (value) {
-			sessionStorage.userData = JSON.stringify(value);
-		},
-		getInfo: function () {
+		} else {
+			return [];
+		}
+
+	};
+
+	this.setInfo = function (value) {
+		sessionStorage.userData = JSON.stringify(value);
+	};
+
+	this.getInfo = function () {
+		if (sessionStorage.userData){
 			return JSON.parse(sessionStorage.userData);
-		},
-		setFiltro: function(value) {
-			sessionStorage.setItem('filtro', value);
-		},
-		getFiltro: function() {
-			return sessionStorage.getItem('filtro');
-		},
-		unsetLogin: function(){
-			sessionStorage.clear();
+		} else {
+			return {};
+		}
+
+	};
+
+	this.setFiltro = function(value) {
+		sessionStorage.setItem('filtro', value);
+		$cookies.put('themeTerminal', value);
+		switch (value){
+			case 'BACTSSA':
+				this.terminalEstilo = 'bootstrap.cerulean';
+				this.logoTerminal = 'images/logo_bactssa.png';
+				break;
+			case 'TERMINAL4':
+				this.terminalEstilo = 'bootstrap.united';
+				this.logoTerminal = 'images/logo_terminal4.png';
+				break;
+			case 'TRP':
+				this.terminalEstilo = 'bootstrap.flaty';
+				this.logoTerminal = 'images/logo_trp.png';
+				break;
 		}
 	};
+
+	this.getFiltro = function() {
+		return sessionStorage.getItem('filtro');
+	};
+
+	this.unsetLogin = function(){
+		sessionStorage.clear();
+		this.setFiltro('BACTSSA');
+	};
+
+	this.setHeaders = function(){
+		$http.defaults.headers.common.token = this.getToken();
+	}
+
 }]);
