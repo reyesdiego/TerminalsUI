a27bfbefb450f30aa30b21bbe1121e2ea46d55a8
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2017-08-30 11:44:16 -0300

    Refactor loginCtrl as a class

--- controller/login/login.controller.js
+++ controller/login/login.controller.js
@@ -1,38 +1,21 @@
 /**
  * Created by Diego Reyes on 1/23/14.
  */
-myapp.controller('loginCtrl', ['$state', 'loginService', 'authFactory', 'dialogs', '$uibModal', 'generalFunctions',
-	function($state, loginService, authFactory, dialogs, $uibModal, generalFunctions) {
-		'use strict';
-		const vm = this;
+class LoginCtrl {
 
-		function cerrarSesion(){
-			authFactory.logout();
-			vm.entrando = false;
-		}
+	constructor($state, loginService, authFactory, dialogs, $uibModal, generalFunctions){
+		this._$state = $state;
+		this._loginService = loginService;
+		this._authFactory = authFactory;
+		this._dialogs = dialogs;
+		this._$uibModal = $uibModal;
+		this._generalFunctions = generalFunctions;
 
-		function errorHandler(error){
-			let errdlg;
-			if (error.code == 'ACC-0010'){
-				errdlg = dialogs.error("Error de acceso", "Su usuario ha sido aprobado, pero aún no se le han asignado permisos a las diferentes partes de la aplicación. Por favor, vuelva a intentarlo más tarde.");
-				errdlg.result.then(() => {
-					cerrarSesion();
-				});
-			} else if (error.code == 'ACC-0003') {
-				$state.transitionTo('validar');
-			} else if (error.code == 'ACC-0001' || error.code == 'ACC-0002' || error.code == 'ACC-0004') {
-				errdlg = dialogs.error("Error de acceso", error.message);
-				errdlg.result.then(() => {
-					cerrarSesion();
-				});
-			}
-		}
+		this.entrando = false;
 
-		vm.entrando = false;
-
-		vm.email = '';
-		vm.password = '';
-		vm.sesion = true;
+		this.email = '';
+		this.password = '';
+		this.sesion = true;
 
 		if (loginService.isLoggedIn){
 			if (generalFunctions.in_array('tarifario', loginService.acceso)){
@@ -41,36 +24,63 @@ myapp.controller('loginCtrl', ['$state', 'loginService', 'authFactory', 'dialogs
 				$state.transitionTo(loginService.acceso[0])
 			}
 		}
+	}
 
-		vm.login = () => {
-			vm.entrando = true;
-			authFactory.userEnter(vm.email, vm.password, vm.sesion).then((result) => {
-				//$rootScope.socket.emit('login', result.user); //TODO incluir esto en el authfactory
-				if (generalFunctions.in_array('tarifario', loginService.acceso)) {
-					$state.transitionTo('tarifario');
-				} else {
-					$state.transitionTo(loginService.acceso[0])
-				}
-			}).catch((error) => {
-				errorHandler(error);
-			});
-		};
+	cerrarSesion(){
+		this._authFactory.logout();
+		this.entrando = false;
+	}
 
-		vm.resetPassword = () => {
-			const modalInstance = $uibModal.open({
-				templateUrl: 'view/login/reset.password.modal.html',
-				controller: 'resetPasswordDialogCtrl',
-				backdrop: 'static'
+	errorHandler(error){
+		let errdlg;
+		if (error.code == 'ACC-0010'){
+			errdlg = this._dialogs.error("Error de acceso", "Su usuario ha sido aprobado, pero aún no se le han asignado permisos a las diferentes partes de la aplicación. Por favor, vuelva a intentarlo más tarde.");
+			errdlg.result.then(() => {
+				this.cerrarSesion();
 			});
-			modalInstance.result.then((email) => {
-				authFactory.resetPassword(email, (data) => {
-					if (data.status == 'OK'){
-						dialogs.notify('Recuperación de contraseña', 'Solicitud enviada correctamente. En breve recibirá un correo en la dirección indicada con su nueva contraseña.');
-					} else {
-						//console.log(data);
-						dialogs.error('Error', 'No se ha encontrado una cuenta asociada a la dirección enviada.')
-					}
-				})
+		} else if (error.code == 'ACC-0003') {
+			this._$state.transitionTo('validar');
+		} else if (error.code == 'ACC-0001' || error.code == 'ACC-0002' || error.code == 'ACC-0004') {
+			errdlg = this._dialogs.error("Error de acceso", error.message);
+			errdlg.result.then(() => {
+				this.cerrarSesion();
 			});
 		}
-	}]);
+	}
+
+	login(){
+		this.entrando = true;
+		this._authFactory.userEnter(this.email, this.password, this.sesion).then((result) => {
+			if (this._generalFunctions.in_array('tarifario', this._loginService.acceso)) {
+				this._$state.transitionTo('tarifario');
+			} else {
+				this._$state.transitionTo(this._loginService.acceso[0])
+			}
+		}).catch((error) => {
+			this.errorHandler(error);
+		});
+	}
+
+	resetPassword(){
+		const modalInstance = this._$uibModal.open({
+			templateUrl: 'view/login/reset.password.modal.html',
+			controller: 'resetPasswordDialogCtrl',
+			backdrop: 'static'
+		});
+		modalInstance.result.then((email) => {
+			this._authFactory.resetPassword(email, (data) => {
+				if (data.status == 'OK'){
+					this._dialogs.notify('Recuperación de contraseña', 'Solicitud enviada correctamente. En breve recibirá un correo en la dirección indicada con su nueva contraseña.');
+				} else {
+					//console.log(data);
+					this._dialogs.error('Error', 'No se ha encontrado una cuenta asociada a la dirección enviada.')
+				}
+			})
+		});
+	}
+
+}
+
+LoginCtrl.$inject = ['$state', 'loginService', 'authFactory', 'dialogs', '$uibModal', 'generalFunctions'];
+
+myapp.controller('loginCtrl', LoginCtrl);
