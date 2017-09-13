354e41fedc62baf6ed22076ced98dccfc290c9ea
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2017-09-13 14:16:13 -0300

    Muevo filtros a archivo correspondiente

--- filter/filters.js
+++ filter/filters.js
@@ -117,3 +117,21 @@ myapp.filter('nombreComprobante', ['cacheService', function(cacheService) {
 		}
 	})
 }]);
+
+myapp.filter('singleDecimal', function ($filter) {
+	return function (input) {
+		if (isNaN(input)) return input;
+		return Math.round(input * 10) / 10;
+	};
+});
+
+myapp.filter('setDecimal', function ($filter) {
+	return function (input, places) {
+		if (isNaN(input)) return input;
+		// If we want 1 decimal place, we want to mult/div by 10
+		// If we want 2 decimal places, we want to mult/div by 100, etc
+		// So use the following to create that factor
+		var factor = "1" + Array(+(places > 0 && places + 1)).join("0");
+		return Math.round(input * factor) / factor;
+	};
+});
