daa586031d919a66fa0e7db573bba9a080e98944
Diego Reyes <reyesdiego@hotmail.com>
2017-09-28 11:44:21 -0300

    Agrego buque viaje en pivot. Tabs

--- controller/reportes/report.container.controller.js
+++ controller/reportes/report.container.controller.js
@@ -3,52 +3,52 @@
  */
 myapp.controller('reporteContenedorCtrl', ['$scope', 'reportsFactory', function($scope, reportsFactory){
 
-	$scope.ranking = true;
+    $scope.ranking = true;
 
-	$scope.model = {
-		fechaInicio: new Date(),
-		fechaFin: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
-		clients: [],
-		terminal: '',
-		top: '20',
-		campo: 'total',
-		order: '-1',
-		normalOrder: true
-	};
+    $scope.model = {
+        fechaInicio: new Date(),
+        fechaFin: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+        clients: [],
+        terminal: '',
+        top: '20',
+        campo: 'total',
+        order: '-1',
+        normalOrder: true
+    };
 
-	$scope.reverse = false;
+    $scope.reverse = false;
 
-	$scope.ocultarFiltros = ['campo'];
+    $scope.ocultarFiltros = ['campo'];
 
-	$scope.mensajeResultado = {
-		titulo: 'Reporte contenedores',
-		mensaje: 'Selecciones los parámetros deseados y presione "Buscar" para generar el reporte.',
-		tipo: 'panel-info'
-	};
+    $scope.mensajeResultado = {
+        titulo: 'Reporte contenedores',
+        mensaje: 'Selecciones los parámetros deseados y presione "Buscar" para generar el reporte.',
+        tipo: 'panel-info'
+    };
 
-	$scope.cargando = false;
+    $scope.cargando = false;
 
-	$scope.resultados = [];
+    $scope.resultados = [];
 
-	function cargarReporte(){
-		$scope.reverse = ($scope.model.order == -1);
-		$scope.cargando = true;
-		reportsFactory.getReporteContainers($scope.model).then(data => {
-			$scope.resultados = data;
-		}).catch(error => {
-			$scope.resultados = [];
-			$scope.mensajeResultado = {
-				titulo: 'Error',
-				mensaje: `Se produjo un error al cargar los datos del reporte.\n${error.message}`,
-				tipo: 'panel-danger'
-			};
-		}).finally(() => {
-			$scope.cargando = false;
-		});
-	}
+    function cargarReporte(){
+        $scope.reverse = ($scope.model.order == -1);
+        $scope.cargando = true;
+        reportsFactory.getReporteContainers($scope.model).then(data => {
+            $scope.resultados = data;
+        }).catch(error => {
+            $scope.resultados = [];
+            $scope.mensajeResultado = {
+                titulo: 'Error',
+                mensaje: `Se produjo un error al cargar los datos del reporte.\n${error.message}`,
+                tipo: 'panel-danger'
+            };
+        }).finally(() => {
+            $scope.cargando = false;
+        });
+    }
 
-	$scope.$on('iniciarBusqueda', () => {
-		cargarReporte();
-	});
+    $scope.$on('iniciarBusqueda', () => {
+        cargarReporte();
+    });
 
 }]);
--- controller/reportes/report.pricelist.grouped.controller.js
+++ controller/reportes/report.pricelist.grouped.controller.js
@@ -4,95 +4,101 @@
 
 class ReporteCuboCtrl {
 
-	constructor(reportsFactory, priceFactory, loginService, dialogs) {
-		this.listadoGrupos = [];
+    constructor(reportsFactory, priceFactory, loginService, dialogs) {
+        this.listadoGrupos = [];
 
-		priceFactory.getGroupPrices().then(groupsData => {
-			this.listadoGrupos = groupsData;
-		}).catch(error => {
-			this.errorTarifario = true;
-		});
+        priceFactory.getGroupPrices().then(groupsData => {
+            this.listadoGrupos = groupsData;
+        }).catch(error => {
+            this.errorTarifario = true;
+        });
 
-		this.dialogs = dialogs;
-		this.reportsFactory = reportsFactory;
-		this.loginService = loginService;
+        this.dialogs = dialogs;
+        this.reportsFactory = reportsFactory;
+        this.loginService = loginService;
 
-		//this.pivotTableFields = {
-		//	"Terminal": (item) => (item.terminal),
-		//	"Total": (item) => (item.total),
-		//	//"Movimiento": (item) => (item.mov ? item.mov : 'Indefinido'),
-		//	"Movimiento": (item) => (item.mov),
-		//	"Tipo": (item) => (item.tipo),
-		//	"Medida": (item) => (item.largo),
-		//	"Año": (item) => (item.anio),
-		//	"Mes": (item) => (item.mes),
-		//	"Altura": (item) => (item.iso2Id)
-		//};
-		this.pivotTableFields = {
-			Terminal: item => item.ter,
-			Total: item => item.tot,
-			//"Movimiento": (item) => (item.mov ? item.mov : 'Indefinido'),
-			Movimiento: item => item.mov,
-			Tipo: item => item.tipo,
-			//Grupo: item => item.tariGrupo,
-			Medida: item => item.largo,
-			Año: item => item.a,
-			Mes: item => item.m,
-			Comprobante: item => item.tComprob,
-			Altura: item => item.iso2Id,
-			Contenedor: item => item.contenedor
-		};
+        //this.pivotTableFields = {
+        //	"Terminal": (item) => (item.terminal),
+        //	"Total": (item) => (item.total),
+        //	//"Movimiento": (item) => (item.mov ? item.mov : 'Indefinido'),
+        //	"Movimiento": (item) => (item.mov),
+        //	"Tipo": (item) => (item.tipo),
+        //	"Medida": (item) => (item.largo),
+        //	"Año": (item) => (item.anio),
+        //	"Mes": (item) => (item.mes),
+        //	"Altura": (item) => (item.iso2Id)
+        //};
+        this.pivotTableFields = {
+            Terminal: item => item.ter,
+            Total: item => item.tot,
+            //"Movimiento": (item) => (item.mov ? item.mov : 'Indefinido'),
+            Movimiento: item => item.mov,
+            Tipo: item => item.tipo,
+            //Grupo: item => item.tariGrupo,
+            Medida: item => item.largo,
+            Año: item => item.a,
+            Mes: item => item.m,
+            Altura: item => item.iso2Id,
+            Contenedor: item => item.contenedor,
+            Buque: item => item.buque,
+            Viaje: item => item.viaje
+        };
 
-		this.tablePivot = {
-			data: [],
-			options: {
-				derivedAttributes: this.pivotTableFields,
-				//hiddenAttributes: ["largo", "terminal", "total", "cantidad", "norma", "code", "anio", "mes", "iso3Id", "iso2Id", "mov", "teus", "tipo"],
-				hiddenAttributes: ["largo", "contenedor", "tComprob", "ter", "tot", "code", "a", "m", "iso3Id", "iso2Id", "mov", "tipo"],
-				rows: ["Terminal", "Movimiento"],
-				cols: ["Medida"],
-				vals: ["Total"],
-				rendererName: "Tabla",
-				aggregatorName: "Suma de moneda"
-			}
-		};
+        this.tablePivot = {
+            data: [],
+            options: {
+                derivedAttributes: this.pivotTableFields,
+                //hiddenAttributes: ["largo", "terminal", "total", "cantidad", "norma", "code", "anio", "mes", "iso3Id", "iso2Id", "mov", "teus", "tipo"],
+                hiddenAttributes: ["largo", "contenedor", "tariGrupo", "buque", "viaje", "ter", "tot", "code", "a", "m", "iso3Id", "iso2Id", "mov", "tipo"],
+                rows: ["Terminal", "Movimiento"],
+                cols: ["Medida"],
+                vals: ["Total"],
+                rendererName: "Tabla",
+                aggregatorName: "Suma de moneda"
+            }
+        };
 
-		this.loadingReporteTarifas = false;
+        this.loadingReporteTarifas = false;
 
-		this.configPanel = {
-			tipo: 'panel-danger',
-			titulo: 'Error',
-			mensaje: 'Error al cargar los datos necesarios para generar el reporte.'
-		};
+        this.configPanel = {
+            tipo: 'panel-danger',
+            titulo: 'Error',
+            mensaje: 'Error al cargar los datos necesarios para generar el reporte.'
+        };
 
-		this.tarifasGraficar = [];
+        this.tarifasGraficar = [];
 
-		this.model = {
-			fechaInicio: new Date(new Date().getFullYear(), new Date().getMonth()),
-			fechaFin: new Date()
-		};
+        this.model = {
+            fechaInicio: new Date(new Date().getFullYear(), new Date().getMonth()),
+            fechaFin: new Date()
+        };
 
-	}
+    }
 
-	armarGraficoTarifas() {
-		this.loadingReporteTarifas = true;
-		this.tarifasGraficar = [];
-		this.listadoGrupos.forEach(grupo => {
-			if (grupo.graficar) this.tarifasGraficar.push(grupo._id)
-		});
+    armarGraficoTarifas() {
+        this.loadingReporteTarifas = true;
+        this.tarifasGraficar = [];
+        this.listadoGrupos.forEach(grupo => {
+            if (grupo.graficar) this.tarifasGraficar.push(grupo._id)
+        });
 
-		this.reportsFactory.getReporteTarifasGroupsPivot(this.model, this.tarifasGraficar).then((data) => {
-			this.tablePivot.data = data.data;
-			this.mostrarGrafico = true;
-		}).catch((err) => {
-			this.mostrarGrafico = false;
-			this.dialogs.error('Reporte Tarifas', err.message);
-		}).finally(() => {
-			this.loadingReporteTarifas = false;
-		});
-	}
+        this.reportsFactory.getReporteTarifasGroupsPivot(this.model, this.tarifasGraficar).then((data) => {
 
-};
+            if (this.tarifasGraficar.length > 0) {
+                this.pivotTableFields.Grupo = item => item.tariGrupo;
+            }
+
+            this.tablePivot.data = data.data;
+            this.mostrarGrafico = true;
+        }).catch((err) => {
+            this.mostrarGrafico = false;
+            this.dialogs.error('Reporte Tarifas', err.message);
+        }).finally(() => {
+            this.loadingReporteTarifas = false;
+        });
+    }
+
+}
 
 ReporteCuboCtrl.$inject = ['reportsFactory', 'priceFactory', 'loginService', 'dialogs'];
 
