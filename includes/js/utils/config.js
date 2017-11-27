d1b6e5e982adb37d149ce5e4139a1de0c5046ca4
Diego Reyes <reyesdiego@hotmail.com>
2017-11-27 16:49:30 -0300

    Configs Varios

---
+++ includes/js/utils/config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by diego on 03/05/16.
+ */
+
+var config = {
+    url: function () {
+        return "http://10.10.0.223:8090"
+    },
+    socket: function () {
+        return "http://10.10.0.223:8090"
+    }
+}
---
+++ includes/js/utils/configProd.js
@@ -0,0 +1,12 @@
+/**
+ * Created by diego on 03/05/16.
+ */
+
+var config = {
+    url: function() {
+        return "http://terminales.puertobuenosaires.gob.ar:8090";
+    },
+    socket: function() {
+        return "http://terminales.puertobuenosaires.gob.ar:8090";
+    }
+}
