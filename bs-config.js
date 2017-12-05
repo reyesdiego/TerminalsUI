ee30cc12cb34bc159fc6257b2c893f3e3c3c583c
Diego Reyes <reyesdiego@hotmail.com>
2017-12-05 13:34:43 -0300

    Config para server-lite in order to remove server logging

---
+++ bs-config.js
@@ -0,0 +1,15 @@
+// server-lite config
+module.exports = {
+    server: {
+      middleware: {
+        0: null     // removes default `connect-logger` middleware
+      }
+    }
+  };
+
+//   open: false
+//   logLevel: "silent",
+//   server: {
+//     middleware: {
+//       0: null
+//     }
